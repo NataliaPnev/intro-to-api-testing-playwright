af0415b533334cc114ffe54c987a9f435af45616
Natalia <nataliapnevskaya@gmail.com>
2025-06-18 00:54:53 +0300

    Add order-dto like in lesson10

--- tests/simple-solution-api.spec.ts
+++ tests/simple-solution-api.spec.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@playwright/test'
 
 import { StatusCodes } from 'http-status-codes'
+import { OrderDto } from './dto/order-dto'
 
 test('get order with correct id should receive code 200', async ({ request }) => {
   // Build and send a GET request to the server
@@ -14,28 +15,25 @@ test('get order with correct id should receive code 200', async ({ request }) =>
 
 test('post order with correct data should receive code 201', async ({ request }) => {
   // prepare request body
-  const requestBody = {
-    status: 'OPEN',
-    courierId: 0,
-    customerName: 'string',
-    customerPhone: 'string',
-    comment: 'string',
-    id: 0,
-  }
+  const requestBody = OrderDto.createOrderWithRandomData()
   // Send a POST request to the server
   const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
     data: requestBody,
   })
   // Log the response status and body
   console.log('response status:', response.status())
-  console.log('response body:', await response.json())
+  console.log('request body:', requestBody)
   expect(response.status()).toBe(StatusCodes.OK)
 })
 
 test('get order with orderId 0 should receive code 400', async ({ request }) => {
   // Build and send a GET request to the server
   const response = await request.get('https://backend.tallinn-learning.ee/test-orders/0')
-  expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
+  const responseBody = await response.json()
+  console.log(await response.json() )
+  expect.soft(response.status()).toBe(StatusCodes.BAD_REQUEST)
+  console.log('check message')
+  expect.soft(responseBody.message).toBe('getById.id: must be greater than or equal to 1')
 })
 
 test('get order with orderId 11 should receive code 400', async ({ request }) => {
@@ -66,5 +64,5 @@ test('post order with incorrect data should receive code 415', async ({ request 
   // Log the response status and body
   console.log('response status:', response.status())
   console.log('response body:', await response.json())
-  expect(response.status()).toBe(StatusCodes.UNSUPPORTED_MEDIA_TYPE)
+  expect.soft(response.status()).toBe(StatusCodes.UNSUPPORTED_MEDIA_TYPE)
 })
